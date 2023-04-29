05566823b9c7f3b34b06211ec4492b8497799d47
Manu <manuporwal98@gmail.com>
2023-04-29 18:09:10 +0530

    added flags

--- cmd/stringer/root.go
+++ cmd/stringer/root.go
@@ -1,26 +1,29 @@
 package stringer
 
 import (
- "fmt"
- "os"
+	"fmt"
+	"os"
 
- "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 )
 
+var version = "0.0.1"
+
 var rootCmd = &cobra.Command{
-    Use:  "stringer",
-    Short: "stringer - a simple CLI to transform and inspect strings",
-    Long: `stringer is a super fancy CLI (kidding)
+	Use:     "stringer",
+	Version: version,
+	Short:   "stringer - a simple CLI to transform and inspect strings",
+	Long: `stringer is a super fancy CLI (kidding)
    
 One can use stringer to modify or inspect strings straight from the terminal`,
-    Run: func(cmd *cobra.Command, args []string) {
+	Run: func(cmd *cobra.Command, args []string) {
 
-    },
+	},
 }
 
 func Execute() {
-    if err := rootCmd.Execute(); err != nil {
-        fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
-        os.Exit(1)
-    }
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
+		os.Exit(1)
+	}
 }
